172ee85ee0009fc2290f3d79cdcf030583ccd7cd
seanpierce <sumler.sean@gmail.com>
2018-06-19 21:28:21 -0700

    Add forien key field to Password model

--- models.py
+++ models.py
@@ -10,13 +10,14 @@ class BaseModel(Model):
     class Meta:
         database = db
 
+class User(BaseModel):
+    username = CharField(max_length = 125, unique = True)
+    password_hash = CharField(max_length = 255)
+
 class Password(BaseModel):
+    user = ForeignKeyField(User, backref='passwords')
     application = CharField(max_length = 255)
     login = CharField(max_length = 255)
     password = CharField(max_length = 255)
     notes = TextField(null = False)
     modified_at = DateTimeField(default = datetime.datetime.now)
-
-class User(BaseModel):
-    username = CharField(max_length = 125, unique = True)
-    password_hash = CharField(max_length = 255)

b1cc8fc05881ca84f73aed49025734cb47b9e630
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-06-27 15:27:48 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     classifiers = [
         'Environment :: Console',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: OS Independent',P
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
